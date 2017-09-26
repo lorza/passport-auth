b68df8ac3628b493b81e4421f7aa95177efdbdd5
Christian Lorza <christianlorza@googlemail.com>
2017-09-26 11:53:29 +0100

    Update config.js

--- app/config.js
+++ app/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    db: "mongodb://christianl:adeladorance1999@ds131320.mlab.com:31320/coinfrenzy",
-    cryptKey: "katyaeggkatyaegg",
-    session_secret: "2g%8DG_v'gk4Q}q^",
-    secret: "5860cbe8a0219d1e8bbb3f3931cc6307382abe9b74132f525208f639cc92076136be68a62b45c933e6deb3b995048747sQ+jRUtqZ9rHoo6w9UH12A=="
+    db: "",
+    cryptKey: "",
+    session_secret: "",
+    secret: ""
     
 }
